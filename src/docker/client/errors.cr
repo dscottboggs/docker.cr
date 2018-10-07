8475b628e9ff0f14d01a1c466027b3e5924e9c25
D. Scott Boggs <scott@tams.tech>
2018-10-07 18:42:20 -0400

    added typed errors

---
+++ src/docker/client/errors.cr
@@ -0,0 +1,41 @@
+module Docker
+  class Client
+    class Exception < ::Exception
+    end
+    class InternalServerError < Exception
+      def initialize(endpoint : String, response : HTTP::Client::Response)
+        super "\
+          Got 'internal server error' from the docker daemon at endpoint \
+          #{endpoint}.\nResponse body: #{response.body?}."
+      end
+    end
+    class BadParameter < Exception
+      def initialize(endpoint : String, parameter : String, parameter_value)
+        super "\
+          got invalid parameter #{parameter} with value #{parameter_value} for \
+          endpoint #{endpoint}."
+      end
+      def initialize(endpoint : String, response : HTTP::Client::Response)
+        super "\
+          Got 'bad parameter' from the docker daemon at endpoint \
+          #{endpoint}.\nResponse body: #{response.body?}."
+      end
+    end
+    class NotFound < Exception
+      def initialize(
+        resource_type : String,
+        endpoint : String,
+        response : HTTP::Client::Response? = nil
+      )
+        super "\
+          The docker daemon couldn't find the #{resource_type} \
+          at #{endpoint}.#{"\nResponse body: #{response.body?}" unless response.nil?}"
+      end
+    end
+    class NotYetImplemented < Exception
+        def initialize(feature : Symbol)
+          super "the feature #{feature.to_s} is not yet implemented."
+        end
+    end
+  end
+end
