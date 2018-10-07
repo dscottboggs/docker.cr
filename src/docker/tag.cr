430e7d7072479f501a9067f0600d1b0f96ef135e
D. Scott Boggs <scott@tams.tech>
2018-10-07 18:43:40 -0400

    Added tag

---
+++ src/docker/tag.cr
@@ -0,0 +1,45 @@
+module Docker
+  # A tag for a particular image
+  class Tag
+    property repo : String?
+    property image_name : String
+    property tag : String
+
+    def initialize(@image_name : String, @tag : String); end
+
+    def initialize(@repo : String?, @image_name : String, @tag : String); end
+
+    def self.from_s(string : String) : self
+      repo, image_name, tag = nil, nil, nil
+      if string.includes?("/") && string.includes?(":")
+        repo, not_repo = string.split "/"
+        image_name, tag = not_repo.split(":")
+      elsif string.includes? ":"
+        image_name, tag = string.split(":")
+      elsif string.includes? "/"
+        repo, image_name = string.split("/")
+        tag = "latest"
+      else
+        image_name = string
+        tag = "latest"
+      end
+      self.new repo, image_name, tag
+    end
+
+    def to_s
+      if repo.nil?
+        image_name + ":" + tag
+      else
+        repo.not_nil! + "/" + image_name + ":" + tag
+      end
+    end
+
+    def to_params
+      String.builder do |path|
+        path << "repo="
+        path << repo << "/" unless repo.nil?
+        path << image_name << "&tag=" << tag
+      end
+    end
+  end
+end
