c4b0cd4318ec18c40288032ab2832464d5f2cb80
D. Scott Boggs <scott@tams.tech>
2018-11-07 06:27:35 -0500

    Added lazy loading to Docker.client

--- src/docker.cr
+++ src/docker.cr
@@ -1,13 +1,15 @@
 require "json"
 require "openssl"
 require "http"
+require "./docker/client/errors"
 require "./core_ext/**"
 require "./docker/*"
 
 module Docker
   extend self
+  @@client : Docker::APIClient
 
   def client
-    Docker::APIClient.new
+    @@client ||= Docker::APIClient.new
   end
 end
